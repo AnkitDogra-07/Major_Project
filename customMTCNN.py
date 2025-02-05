bc7a88962fda630785801b82c35625f94b2c93f8
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2025-02-05 20:28:51 +0530

    >..<

--- customMTCNN.py
+++ customMTCNN.py
@@ -5,3 +5,8 @@ import numpy as np
 from torchvision import transforms
 from typing import Tuple, List
 from customMTCNN import CustomMTCNN
+
+class PNet(nn.module):
+    """Proposal Network (P-Net) - First stage of MTCNN"""
+    def __init__(self):
+        super(PNet, self).__init__()
